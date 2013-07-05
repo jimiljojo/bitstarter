39832ab9fb09791b467fd22da5d9e75a23e8c605
Jimil Patel <jimiljojo@gmail.com>
2013-07-05 18:13:22 +0000

    added index.html to web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buf = new Buffer(8);
-  buf = fs.readFileSync('index.html);
+  buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
